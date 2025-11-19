e71bdf427c3376b5d264537580b3c996cce896c2
abhijithx <abhijithsudhakaran123@gmail.com>
2025-11-19 15:49:00 +0530

    graphics added

---
+++ CO5-Files/CO4 -Graphics/program1.py
@@ -0,0 +1,14 @@
+import turtle
+
+t = turtle.Turtle()
+t.speed(-100)
+t.pensize(20)
+turtle.bgcolor("black")
+
+colors = ["red", "yellow"]
+for i in range(400):
+    t.color(colors[i % 2]) 
+    t.circle(100)
+    t.right(10)
+
+turtle.done()
